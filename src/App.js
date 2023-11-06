f3cbb1c0bc490b004c0623f89abaadf5204f4296
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-11-06 17:24:12 +0330

    create modals for edit,delete,details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Sidebar from './components/sidebar/Sidebar';
+import Header from './components/header/Header';
+import routes from './routes';
+import { useRoutes } from 'react-router-dom';
 export default function App() {
+  const router = useRoutes(routes)
   return (
-    <div className="App text-danger">
-        Hello
+    <div className="app d-flex justify-content-between w-100">
+    <Sidebar />
+
+    <div className="main">
+      <Header />
+      {router}
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './custom.css'
+import { BrowserRouter } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 );
